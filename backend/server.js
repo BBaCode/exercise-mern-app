6d05bc0a371bc28b0cb535e0f56a59679d1f42bb
Brian Bassett <brianbassett@Brians-MacBook-Pro.local>
2022-04-14 13:58:43 -0400

    created react app and built out basic backend functionality

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 5050;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connected successfully");
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
